79fa25651392f4c982e3b7dc118b999259b7ab5b
altynai2002 <altynay.bakytbekova.02@mail.ru>
2022-06-01 08:18:33 +0600

    simple dice roller

--- app/src/main/java/com/pro/androidsummer/MainActivity.kt
+++ app/src/main/java/com/pro/androidsummer/MainActivity.kt
@@ -2,10 +2,30 @@ package com.pro.androidsummer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
+import androidx.appcompat.widget.AppCompatTextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var diceBtn: AppCompatButton
+    private lateinit var diceText: AppCompatTextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        diceBtn = findViewById(R.id.diceButton)
+        diceBtn.setOnClickListener {
+            Toast.makeText(this, "dice starts rolling", Toast.LENGTH_SHORT).show()
+            rollDice() }
+    }
+
+    private fun rollDice() {
+        diceText = findViewById(R.id.diceTxt)
+        val randInt = (1..6).random()
+        Thread.sleep(2000)
+        diceText.text = randInt.toString()
     }
 }

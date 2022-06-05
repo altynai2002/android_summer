f7efe354d8e7ccc107ee5f9b1eac063e883063f4
altynai2002 <altynay.bakytbekova.02@mail.ru>
2022-06-05 17:48:31 +0600

    added binding

--- app/src/main/java/com/pro/androidsummer/MainActivity.kt
+++ app/src/main/java/com/pro/androidsummer/MainActivity.kt
@@ -6,26 +6,28 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
 import androidx.appcompat.widget.AppCompatTextView
+import com.pro.androidsummer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var diceBtn: AppCompatButton
-    private lateinit var diceText: AppCompatTextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        diceBtn = findViewById(R.id.diceButton)
-        diceBtn.setOnClickListener {
-            Toast.makeText(this, "dice starts rolling", Toast.LENGTH_SHORT).show()
-            rollDice() }
+        binding.apply {
+            diceButton.setOnClickListener {
+                rollDice()
+            }
+        }
+
     }
 
     private fun rollDice() {
-        diceText = findViewById(R.id.diceTxt)
         val randInt = (1..6).random()
         Thread.sleep(2000)
-        diceText.text = randInt.toString()
+        binding.diceTxt.text = randInt.toString()
     }
 }

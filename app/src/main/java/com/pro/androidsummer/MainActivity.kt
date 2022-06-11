2752289f3cdac1ea1aa8772afcbd5fc7222dc082
altynai2002 <altynay.bakytbekova.02@mail.ru>
2022-06-11 12:10:16 +0600

    added images

--- app/src/main/java/com/pro/androidsummer/MainActivity.kt
+++ app/src/main/java/com/pro/androidsummer/MainActivity.kt
@@ -2,15 +2,14 @@ package com.pro.androidsummer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.widget.AppCompatButton
-import androidx.appcompat.widget.AppCompatTextView
+import androidx.appcompat.widget.AppCompatImageView
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var diceBtn: AppCompatButton
-    private lateinit var diceText: AppCompatTextView
+    private lateinit var diceImg: AppCompatImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,14 +17,21 @@ class MainActivity : AppCompatActivity() {
 
         diceBtn = findViewById(R.id.diceButton)
         diceBtn.setOnClickListener {
-            Toast.makeText(this, "dice starts rolling", Toast.LENGTH_SHORT).show()
             rollDice() }
     }
 
     private fun rollDice() {
-        diceText = findViewById(R.id.diceTxt)
+        diceImg = findViewById(R.id.img)
         val randInt = (1..6).random()
-        Thread.sleep(2000)
-        diceText.text = randInt.toString()
+        Thread.sleep(1500)
+        val drawableResource = when (randInt) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            else -> R.drawable.dice_6
+        }
+        diceImg.setImageResource(drawableResource)
     }
 }
